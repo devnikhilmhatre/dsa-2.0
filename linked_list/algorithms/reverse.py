e13c9f1c6a47ef7eaa3b9e5ecffc2f086c86c15a
Nikhil Mhatre <nikhil.mhatre93@gmail.com>
2025-05-11 22:32:00 +0530

    liked list, find middle

--- linked_list/algorithms/reverse.py
+++ linked_list/algorithms/reverse.py
@@ -1,14 +1,9 @@
-from linked_list.singly import Singly
 from linked_list.node import Data
-
-data = (Data(i) for i in range(1, 6))
+from linked_list.algorithms import get_singly
 
 
 def reverse():
-    singly = Singly()
-    for item in data:
-        singly.append(item)
-
+    singly = get_singly()
     singly.represent()
     singly.delete(Data(2))
     singly.represent()
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-from linked_list.algorithms.reverse import reverse
+from linked_list.algorithms.find_middle import find_middle_count, find_middle_fast_slow
 
-reverse()
+find_middle_count()
+print("-" * 20)
+find_middle_fast_slow()
